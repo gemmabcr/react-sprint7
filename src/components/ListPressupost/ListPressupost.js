48f604bb168f97436ba250d79e83de53e0b3989b
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-05 20:46:00 +0100

    feat: show data of item list

--- src/components/ListPressupost/ListPressupost.js
+++ src/components/ListPressupost/ListPressupost.js
@@ -8,15 +8,27 @@ const ListPressupost = ({ title, listPressupost }) => {
       <h4>{ title }</h4>
       { listPressupost.length > 0 && listPressupost.map(item =>
         <ItemList key={ item.id }>
+          <p>{ item.date }</p>
           <p>{ item.title }, de { item.client }</p>
           { item.products.length > 0 &&
-            <p>{ JSON.stringify(item.products) }</p>
+            <div>
+              <p>Productos</p>
+              <ul>
+                { item.products.map(product =>
+                  <div key={product.name}>
+                    <li>{ product.name }: { product.price }€</li>
+                    { product.id === 'web' && item.webFunctions.length > 0 && item.webFunctions.map(webFunction =>
+                      <ul key={webFunction.id}>
+                        <li>{ webFunction.name }: { webFunction.number } = { webFunction.number*30}€</li>
+                      </ul>
+                    ) }
+                  </div>
+                ) }
+              </ul>
+
+            </div>
           }
-          { item.webFunctions.length > 0 &&
-            <p>{ JSON.stringify(item.webFunctions) }</p>
-          }
-          <p>Precio total: { item.totalPrice }</p>
-          <p>{ item.date }</p>
+          <p>Precio total: { item.totalPrice }€</p>
         </ItemList>
       )}
     </FlexColumn>

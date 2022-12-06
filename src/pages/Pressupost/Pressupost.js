e856e6ac0b2fb773461d8620692d00f1f00c63ad
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-06 18:11:56 +0100

    feat: extracted functions from pressupost page

--- src/pages/Pressupost/Pressupost.js
+++ src/pages/Pressupost/Pressupost.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 import { PressupostContainer } from './PressupostStyled'
 import ListBudget from '../../components/ListPressupost/ListBudget'
 import Modal from '../../components/Modal/Modal'
-import { findWebOption, format, getListData, getNewFormData, saveToLocal } from './PressupostFunctions'
+import {
+  calculateTotal,
+  getListData,
+  getNewFormData,
+  getSubmitData,
+  saveToLocal
+} from './PressupostFunctions'
 import FormPressupost from '../../components/FormPressupost'
 import { newFormData as importedData } from '../../data'
 
@@ -17,45 +23,11 @@ const Pressupost = () => {
 
   React.useEffect(() => {
     const selectedOptions = formData.filter(item => item.selected)
-    setTotal(prevTotal => {
-      let total = 0
-      if (selectedOptions.length > 0) {
-        for (let option of selectedOptions) {
-          total += option.price
-        }
-        const webOption = formData.find(item => item.id === 'web')
-        if (!!webOption.selected) {
-          const webFunctionalities = formData.filter(item => item.webConditional)
-          for (let functionality of webFunctionalities) {
-            const totalFunctionality = functionality.value * 30
-            total += totalFunctionality
-          }
-        }
-      }
-      return total
-    })
+    setTotal(calculateTotal(selectedOptions, formData))
   }, [formData])
 
   function onNewSubmit () {
-    const namePressupost = formData.find(item => item.id === 'title')
-    const nameClient = formData.find(item => item.id === 'client')
-    const selectedItems = formData.filter(item => item.selected)
-    const webOption = findWebOption(selectedItems)
-    let webFunctionalities = []
-    if (webOption !== undefined) {
-      const pages = formData.find(item => item.id === 'pages')
-      const languages = formData.find(item => item.id === 'languages')
-      webFunctionalities = [pages, languages]
-    }
-    const newListItem = {
-      id: listPressupost.length,
-      title: namePressupost.value,
-      client: nameClient.value,
-      products: selectedItems,
-      webFunctions: webFunctionalities,
-      totalPrice: total,
-      date: format(new Date())
-    }
+    const newListItem = getSubmitData(formData, listPressupost, total)
     setListPressupost(prevList => {
       const newListData = [...prevList, newListItem]
       saveToLocal('listPressupost', newListData)
--- src/pages/Pressupost/PressupostFunctions.js
+++ src/pages/Pressupost/PressupostFunctions.js
@@ -32,6 +32,24 @@ export function findWebOption (selectedItems) {
   return selectedItems.find(item => item.id === 'web')
 }
 
+export function calculateTotal (options, data) {
+  let total = 0
+  if (options.length > 0) {
+    for (let option of options) {
+      total += option.price
+    }
+    const webOption = data.find(item => item.id === 'web')
+    if (!!webOption.selected) {
+      const webFunctionalities = data.filter(item => item.webConditional)
+      for (let functionality of webFunctionalities) {
+        const totalFunctionality = functionality.value * 30
+        total += totalFunctionality
+      }
+    }
+  }
+  return total
+}
+
 export function format (inputDate) {
   let date = inputDate.getDate()
   let month = inputDate.getMonth() + 1
@@ -46,4 +64,26 @@ export function format (inputDate) {
     .padStart(2, '0')
 
   return `${date}/${month}/${year}`
+}
+
+export function getSubmitData (data, list, total) {
+  const budget = data.find(item => item.id === 'title')
+  const client = data.find(item => item.id === 'client')
+  const selectedItems = data.filter(item => item.selected)
+  const webOption = findWebOption(selectedItems)
+  let webFunctionalities = []
+  if (webOption !== undefined) {
+    const pages = data.find(item => item.id === 'pages')
+    const languages = data.find(item => item.id === 'languages')
+    webFunctionalities = [pages, languages]
+  }
+  return {
+    id: list.length,
+    title: budget.value,
+    client: client.value,
+    products: selectedItems,
+    webFunctions: webFunctionalities,
+    totalPrice: total,
+    date: format(new Date())
+  }
 }
